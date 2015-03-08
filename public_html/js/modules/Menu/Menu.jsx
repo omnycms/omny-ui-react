f755910b66a030bdfd3952caca3c386ab6b3355f
Al Lamarre <alamarre@gmail.com>
2015-03-08 13:00:00 -0400

    create menu and some cleanup

---
+++ public_html/js/modules/Menu/Menu.jsx
@@ -0,0 +1,33 @@
+define(["utilities/OmnyApiRequester","utilities/ModuleManager"],
+    function(apiRequester,moduleManager) {
+      var OmnyMenu = React.createClass({
+          getInitialState: function() {
+            return {menuItems: []};
+          },
+          componentDidMount: function(){
+            var menu = this;
+            apiRequester.apiRequest("menus","default/entries",{
+              success:function(menuItems) {
+                menu.setState({menuItems: menuItems});
+              }
+            });
+          },
+          render: function() {
+              return <ul>
+                 {this.state.menuItems.map(function(menuItem, i){
+                      return <li key={i}>
+                        <a href={menuItem.link}>menuItem.title</a>
+                        </li>;
+                  })}
+              </ul>;
+            }
+      });
+
+      function Menu(data) {
+          this.render = function(element) {
+              React.render(<OmnyMenu />, element);
+          }
+      }
+      return Menu;
+    }
+);
