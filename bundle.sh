a861c35e159c00ad2ca9c18a578830f4f3038f2d
alamarre <alamarre@gmail.com>
2015-07-02 00:24:22 -0400

    Publish in proper folder

--- bundle.sh
+++ bundle.sh
@@ -1,6 +1,8 @@
 npm install requirejs
 
-node node_modules/requirejs/bin/r.js -o build.js
+mkdir output
 
-cp public_html/js/lib output/js/lib -r
+cp public_html output/$1 -r
+node node_modules/requirejs/bin/r.js -o build.js
+mv output/app.js output/$1/app.js -f
 cp public_html/index.html output/index.html
