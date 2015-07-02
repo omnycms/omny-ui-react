2bd90ab3a1dd0109bf8c7382492e1234463c6f9a
alamarre <alamarre@gmail.com>
2015-07-02 00:29:11 -0400

    Fix app.js location

--- bundle.sh
+++ bundle.sh
@@ -4,5 +4,5 @@ mkdir output
 
 cp public_html output/$1 -r
 node node_modules/requirejs/bin/r.js -o build.js
-mv output/app.js output/$1/app.js -f
+mv output/js/app.js output/$1/js/app.js -f
 cp public_html/index.html output/index.html
