d2f6df0e7f443ba8cb1c6c059fb63fec352faddf
alamarre_2 <a.lamarre@gmail.com>
2015-07-07 21:50:25 +0000

    install mustache

--- bundle.sh
+++ bundle.sh
@@ -6,5 +6,5 @@ cp public_html/ output/$1 -r
 rm -rf output/$1/jsx
 node node_modules/requirejs/bin/r.js -o build.js
 mv output/js/app.js output/$1/js/app.js -f
-
+npm install -g mustache
 mustache rewrite.json public_html/index.html > output/$1/index.html
