031e10d3716af373c9020c9e890addae7dc6eaaf
alamarre_2 <a.lamarre@gmail.com>
2015-07-22 00:58:34 +0000

    remove dependencies that are brought into compiled app.js

--- bundle.sh
+++ bundle.sh
@@ -7,4 +7,4 @@ rm -rf output/$1/jsx
 node node_modules/requirejs/bin/r.js -o build.js
 mv output/js/app.js output/$1/js/app.js -f
 npm install -g mustache
-mustache rewrite.json public_html/index.html > output/$1/index.html
+mustache rewrite.json public_html/index-minified.html > output/$1/index.html
